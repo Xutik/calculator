25657343e1e438f8501faf49c293b173d12f0205
Xutik <kseniia.prakhova@hyperisland.se>
2024-09-18 16:01:49 +0200

    2 step

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-let numberOne = 1
-let numberTwo = 2
+console.log('test')
+
+let firstNumber
+let secondNumber
+let operation
+
+document.getNumber('display').addEventListener()
+function getNumber() {
+    const numberInput = document.getElementById('display');
+    const number = Number(numberInput.value);
+    
+  }
+
+
+document.getElementById('display').addEventListener()
+const display = document.getElementById('display');
+function updateDisplay(value) {
+    document.getElementById('display').value = value;
+  }
+
+document.getElementById('delete').addEventListener('click', deleteLastChar);
+function deleteLastChar() {
+    let display = document.getElementById('display'); 
+    let currentValue = display.value;
+    
+    if (currentValue.length > 0) {
+    display.value = currentValue.slice(0, -1);
+    }
+  }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const value = this.textContent;
+    display.value += value;
+  });
+});
+
